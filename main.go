79eb2994d324fbe1cbddfd9357cf1616d210e611
pra <pradeepdeeti4446@gmail.com>
2026-04-18 14:30:39 +0530

    fixed security issues

--- main.go
+++ main.go
@@ -91,6 +91,10 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadHeaderTimeout: 5 * time.Second,
+    		ReadTimeout: 10 * time.Second,
+    		WriteTimeout: 10 * time.Second,
+    		IdleTimeout: 120 * time.Second,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
